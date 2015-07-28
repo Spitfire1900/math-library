4e66245f8bb1483af378845d69dccb5922401fe6
Kyle Gottfried <spitfire1900@live.com>
2015-07-28 00:45:50 -0400

    code queue feature, errors and inefficent. Perhaps a thread array

--- MathLibrary/PrimesListBuilder.cs
+++ MathLibrary/PrimesListBuilder.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MathLibrary
 {
@@ -26,6 +27,7 @@ namespace MathLibrary
         private long _startingValue = 0;
         private long _maxValue = Int64.MaxValue;
         private long _maxTime = 120;
+        private uint _threads = 4;
 
         #endregion
 
@@ -89,6 +91,15 @@ namespace MathLibrary
                     _numOfPrimes = value;
             }
         }
+        public uint Threads
+        {
+            get { return _threads; }
+            set
+            {
+                if (value != 0)
+                    _threads = value;
+            }
+        }
 
         #endregion
 
@@ -107,12 +118,32 @@ namespace MathLibrary
         { NumOfPrimes = numOfPrimes; }
 
         /// <summary>
+        /// Overloaded Constructor. FindPrimes() will operate for maximum 2 minutes.
+        /// </summary>
+        /// <param name="numOfPrimes">Number of Primes to return. (0=Inifinte)</param>
+        /// <param name="threads">Number of threads to use to calculate primes</param>
+        public PrimesListBuilder(int numOfPrimes, uint threads)
+        {
+            NumOfPrimes = numOfPrimes;
+            Threads = threads;
+        }
+
+        /// <summary>
         /// Overloaded Constructor.
         /// </summary>
         /// <param name="numOfPrimes">Number of Primes to return. (0=Inifinte)</param>
         /// <param name="maxTime">Maximum time that FindPrimes() should be permitted to run (in Seconds).</param>
         public PrimesListBuilder(int numOfPrimes, long maxTime)
         { NumOfPrimes = numOfPrimes; MaxTime = maxTime; }
+
+        /// <summary>
+        /// Overloaded Constructor.
+        /// </summary>
+        /// <param name="numOfPrimes">Number of Primes to return. (0=Inifinte)</param>
+        /// <param name="threads">Number of threads to use to calculate primes</param>
+        /// <param name="maxTime">Maximum time that FindPrimes() should be permitted to run (in Seconds).</param>
+        public PrimesListBuilder(int numOfPrimes, uint threads, long maxTime)
+        { NumOfPrimes = numOfPrimes; Threads = Threads; MaxTime = maxTime; }
 
         /// <summary>
         /// Overloaded Constructor.
@@ -127,11 +158,32 @@ namespace MathLibrary
         /// Overloaded Constructor.
         /// </summary>
         /// <param name="numOfPrimes">Number of Primes to return. (0=Inifinte)</param>
+        /// <param name="threads">Number of threads to use to calculate primes</param>
+        /// <param name="maxTime">Maximum time that FindPrimes() should be permitted to run (in Seconds).</param>
+        /// <param name="startingValue">Value at which to start looking for primes.</param>
+        public PrimesListBuilder(int numOfPrimes, uint threads, long maxTime, long startingValue)
+        { NumOfPrimes = numOfPrimes; Threads = threads; MaxTime = maxTime; StartingValue = startingValue; }
+
+        /// <summary>
+        /// Overloaded Constructor.
+        /// </summary>
+        /// <param name="numOfPrimes">Number of Primes to return. (0=Inifinte)</param>
         /// <param name="maxTime">Maximum time that FindPrimes() should be permitted to run (in Seconds).</param>
         /// <param name="startingValue">Value at which to start looking for primes.</param>
         /// <param name="maxValue">Value at which, when reached, to stop looking for primes.</param>
         public PrimesListBuilder(int numOfPrimes, long maxTime, long startingValue, long maxValue)
         { NumOfPrimes = numOfPrimes; MaxTime = maxTime; StartingValue = startingValue; MaxValue = maxValue; }
+
+        /// <summary>
+        /// Overloaded Constructor.
+        /// </summary>
+        /// <param name="numOfPrimes">Number of Primes to return. (0=Inifinte)</param>
+        /// <param name="threads">Number of threads to use to calculate primes</param>
+        /// <param name="maxTime">Maximum time that FindPrimes() should be permitted to run (in Seconds).</param>
+        /// <param name="startingValue">Value at which to start looking for primes.</param>
+        /// <param name="maxValue">Value at which, when reached, to stop looking for primes.</param>
+        public PrimesListBuilder(int numOfPrimes, uint threads, long maxTime, long startingValue, long maxValue)
+        { NumOfPrimes = numOfPrimes; Threads = threads; MaxTime = maxTime; StartingValue = startingValue; MaxValue = maxValue; }
 
         #endregion
 
@@ -142,29 +194,69 @@ namespace MathLibrary
             List<long> listOfPrimes = new List<long>();
             var stopwatch = new System.Diagnostics.Stopwatch(); stopwatch.Start();
 
-            long w = StartingValue;
+            Queue<long> nextPrime = new Queue<long>();
 
-            while (w <= MaxValue && listOfPrimes.Count < NumOfPrimes && stopwatch.ElapsedMilliseconds <= MaxTime * 1000)
-            {
-                long i = 2;
-                bool isPrime = true;
-
-                while (i <= System.Math.Sqrt(w))
+            for (int i = 0; i < Threads - 1; i++)
                 {
-                    if (stopwatch.ElapsedMilliseconds >= MaxTime * 1000)
-                        break;
+                nextPrime.Enqueue(StartingValue + i);
+            }
 
-                    if (w % i == 0)
-                    { isPrime = false; break; }
-                    i++;
+            while (nextPrime.ElementAt(0) <= MaxValue && listOfPrimes.Count < NumOfPrimes && stopwatch.ElapsedMilliseconds <= MaxTime * 1000)
+            {
+
+                List<long> batch = new List<long>();
+                for (int i = 0; i < Threads - 1; i++)
+                {
+                    batch.Add(nextPrime.Dequeue());
                 }
-                
-                if (isPrime == true)
-                    listOfPrimes.Add(w);
-                w++;
+
+                Task<bool> t1 = new Task<bool>(IsPrime(batch[0]));
+                Task<bool> t2 = new Task<bool>(IsPrime(batch[1]));
+                Task<bool> t3 = new Task<bool>(IsPrime(batch[2]));
+                Task<bool> t4 = new Task<bool>(IsPrime(batch[3]));
+
+                t1.Start();
+                t2.Start();
+                t3.Start();
+                t4.Start();
+
+                t1.Wait();
+                t2.Wait();
+                t3.Wait();
+                t4.Wait();
+
+                if (t1.Result == true)
+                    listOfPrimes.Add(batch[0]);
+                if (t2.Result == true)
+                    listOfPrimes.Add(batch[1]);
+                if (t3.Result == true)
+                    listOfPrimes.Add(batch[2]);
+                if (t4.Result == true)
+                    listOfPrimes.Add(batch[3]);
+
+                for (int i = 0; i < Threads - 1; i++)
+                {
+                    nextPrime.Enqueue(batch[3] + i + 1);
+                }
             }
 
             return listOfPrimes;
+        }
+
+        private bool IsPrime(long number)
+        {
+            long i = 2;
+            bool isPrime = true;
+
+            while (i <= System.Math.Sqrt(number))
+            {
+
+                if (number % i == 0)
+                { isPrime = false; break; }
+                i++;
+            }
+
+            return isPrime;
         }
 
         #endregion
